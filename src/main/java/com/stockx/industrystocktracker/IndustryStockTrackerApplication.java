4ea3f14145e9c53f59c4d176af65f5c83f0ee08e
wanger26 <jwanger@uwo.ca>
2021-10-27 15:52:07 -0400

    Inital upload

---
+++ src/main/java/com/stockx/industrystocktracker/IndustryStockTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stockx.industrystocktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryStockTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryStockTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockx/industrystocktracker/IndustryStockTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockx.industrystocktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryStockTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
