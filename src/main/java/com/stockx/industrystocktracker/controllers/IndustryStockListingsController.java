b3d893b824daed3f99cee3dc60724bcce5a581ca
wanger26 <jwanger@uwo.ca>
2021-12-03 10:26:56 -0500

    Fixed CORS issues

--- src/main/java/com/stockx/industrystocktracker/controllers/IndustryStockListingsController.java
+++ src/main/java/com/stockx/industrystocktracker/controllers/IndustryStockListingsController.java
@@ -4,6 +4,7 @@ import com.stockx.industrystocktracker.IndustryStockListing;
 import com.stockx.industrystocktracker.services.IndustryStockListingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class IndustryStockListingsController {
     @Autowired
     private IndustryStockListingsService industryStockListingsService;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/industry-stock-listings")
     public IndustryStockListing getStockByTicker(@RequestParam("sector") String sector) {
 
